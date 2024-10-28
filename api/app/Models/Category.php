f700763250fe52eb173c9f2a8a9b5674992f357d
yey10 <yeyorejuela@gmail.com>
2024-10-28 18:10:59 -0500

    "Updated Category and Role models, added axios and react-router-dom dependencies, and modified package-lock.json and package.json files."

--- api/app/Models/Category.php
+++ api/app/Models/Category.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'id_category';
+
+    public function stories()
+    {
+        return $this->belongsToMany(Story::class, 'category_story', 'id_story');
+    }
 }
--- api/app/Models/Role.php
+++ api/app/Models/Role.php
@@ -4,15 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
-class Category extends Model
+class Role extends Model
 {
     use HasFactory;
 
-    protected $primaryKey = 'id_category';
+    // Asegúrate de que el nombre de la tabla sea correcto
+    protected $table = 'roles'; // Nombre de la tabla
 
-    public function stories()
+    // Establece la clave primaria
+    protected $primaryKey = 'id_rol'; // Mantén esto como 'id_rol' si es el campo autoincremental
+
+    // Si no estás utilizando timestamps, establece esto en false
+    public $timestamps = false;
+
+    // Campos que son rellenables
+    protected $fillable = [
+        'name_rol',
+    ];
+
+    // Definir la relación con usuarios
+    public function users()
     {
-        return $this->belongsToMany(Story::class, 'category_story', 'id_story');
+        return $this->belongsToMany(User::class, 'role_user', 'id_rol', 'id_user');
     }
+
+    // Otros métodos o propiedades personalizadas pueden ir aquí
 }
