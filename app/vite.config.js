c45d07643491a297519ecb56d0b15f19db16ece5
Yerson Orejuela <yeyorejuela@gmail.com>
2024-12-03 15:35:38 -0500

    deployment twice

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: parseInt(process.env.PORT) || 4173, // Usar el puerto proporcionado por Railway
+    host: true, // Permite acceso desde cualquier interfaz
+  },
   plugins: [react()],
   base: "/"
 })
