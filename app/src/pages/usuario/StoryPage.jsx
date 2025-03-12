aa47dba1345aa77f1c54a680cf4520f4ad39bb05
kebinmanco <kebinmanco310@gmail.com>
2025-03-12 07:35:45 -0500

    organizacion visual de botones de historias

--- app/src/pages/usuario/StoryPage.jsx
+++ app/src/pages/usuario/StoryPage.jsx
@@ -147,21 +147,35 @@ const StoryPage = () => {
                       </div>
                       <div className="info-story">
                         <div>
-                        <div>
-                          <h1>Detalles de la Historia</h1>
-                          {/* Botón de Like */}
-                          <button 
-                              onClick={handleLike} 
-                              disabled={isLiking}
-                              className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 disabled:bg-gray-400"
-                          >
-                              <Heart fill={likes[id] ? "red" : "none"} color="black" />
-                              <span>{likes[id] || 0}</span>
-                          </button>
-                        </div>
-                          <div><MessageSquareMore /><p>0</p></div>
-                          <div><Bookmark /><p>0</p></div>
-                          <div><Download /><p>0</p></div>
+                          <div className="buttom buttonLike">
+                            {/* Botón de Like */}
+                            <button 
+                                onClick={handleLike} 
+                                disabled={isLiking}
+                                className=" flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 disabled:bg-gray-400"
+                            >
+                                <Heart fill={likes[id] ? "red" : "none"} />
+                                <p>{likes[id] || 0}</p>
+                            </button>
+                          </div>
+                          <div className="buttom buttonComms">
+                            <button>
+                              <MessageSquareMore />
+                              <p>0</p>
+                            </button>
+                          </div>
+                          <div className="buttom buttonFav">
+                            <button>
+                              <Bookmark />
+                              <p>0</p>
+                            </button>
+                          </div>
+                          <div className="buttom buttonDown">
+                            <button>
+                              <Download />
+                              <p>0</p>
+                            </button>
+                          </div>
                         </div>
                         <div>
                         <p>{story.sinopsis}</p>
