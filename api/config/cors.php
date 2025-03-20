638a79503aa6344d29ff37346ab1966efea31a17
Yerson Orejuela <yeyorejuela@gmail.com>
2025-03-20 08:17:43 -0500

    deploy test

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+         'storysphere-production.up.railway.app'
+        ],
 
     'allowed_origins_patterns' => [],
 
