f3404bb93c641c39d7be8a47ad71b8a3a6993fd3
Yerson Orejuela <yeyorejuela@gmail.com>
2025-03-24 22:27:22 -0500

    test 2 images

--- api/app/Services/Stories/StoryService.php
+++ api/app/Services/Stories/StoryService.php
@@ -17,7 +17,7 @@ class StoryService
         
         foreach ($stories as $story) {
             if ($story->photo && !Str::startsWith($story->photo, ['http', 'https'])) {
-                $story->photo = url(Storage::url($story->photo)); 
+                $story->photo = asset("storage/" . $story->photo);
             }
         }
 
@@ -96,7 +96,8 @@ class StoryService
         }
         $file_name = Str::random(10) . '.' . $file->getClientOriginalExtension();
         $path = Storage::disk('public')->putFileAs('stories_photos', $file, $file_name);
-        return $path ? config('app.url') . Storage::url($path) : null;
+        return $path ? asset("storage/$path") : null;
+
     }   
 
 
