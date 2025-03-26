cfaf9c269eb273a2a559e54cd6194329700b591e
Yerson Orejuela <yeyorejuela@gmail.com>
2025-03-26 14:58:28 -0500

    likes modified

--- app/src/components/Story/StoryHeader.jsx
+++ app/src/components/Story/StoryHeader.jsx
@@ -4,7 +4,7 @@ import ReactStars from "react-rating-stars-component";
 import autorImg from '../../assets/img/autor.jpg';
 import { Heart, MessageSquareMore, Bookmark, Download } from "lucide-react";
 
-const StoryHeader = ({ story, likes, favorites, handleLike, handleFavorite, userInteraction, userRatings, ratings, id, handleRateStory }) => {
+const StoryHeader = ({ story, likes, favorites, handleLike, handleFavorite, userInteractions, userRatings, ratings, id, handleRateStory }) => {
   return (
     <div className="read-info">
       <div>
@@ -36,13 +36,11 @@ const StoryHeader = ({ story, likes, favorites, handleLike, handleFavorite, user
           <div className="info-story">
             <div>
               <div className="buttom buttonLike">
-                <button
-                  onClick={handleLike}
-                  className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 disabled:bg-gray-400"
-                >
-                  <Heart fill={userInteraction?.liked ? "red" : "none"} />
-                  <p>{likes}</p>
-                </button>
+              <button onClick={handleLike} className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+                <Heart fill={userInteractions?.[id] === "like" || userInteractions?.[id] === "both" ? "red" : "none"}
+                  color="red" />
+                <p>{likes}</p>
+              </button>
               </div>
               <div className="buttom buttonComms">
                 <button>
@@ -51,13 +49,11 @@ const StoryHeader = ({ story, likes, favorites, handleLike, handleFavorite, user
                 </button>
               </div>
               <div className="buttom buttonFav">
-                <button
-                  onClick={handleFavorite}
-                  className="flex items-center gap-2 bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600"
-                >
-                  <Bookmark fill={userInteraction?.favorited ? "gold" : "none"}/>
-                  <p>{favorites}</p>
-                </button>
+              <button onClick={handleFavorite} className="flex items-center gap-2 bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600">
+                <Bookmark fill={userInteractions?.[id] === "favorite" || userInteractions?.[id] === "both" ? "gold" : "none"}
+                  color="gold" />
+                <p>{favorites}</p>
+              </button>
               </div>
               <div className="buttom buttonDown">
                 <button>
